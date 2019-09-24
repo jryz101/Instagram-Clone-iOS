c0dfbb316d495208d7e09f1e11de752ba8312bd2
JerryTan <jryz101@gmail.com>
2019-09-24 20:15:38 +0800

    1. Install 'Parse' SDK to the project. 1.1. Import 'Parse' framework and setup 'Parse Server' configuration in AppDelegate.

--- Instagram Clone iOS/AppDelegate.swift
+++ Instagram Clone iOS/AppDelegate.swift
@@ -1,12 +1,11 @@
-//
 //  AppDelegate.swift
 //  Instagram Clone iOS
-//
 //  Created by Jerry Tan on 24/09/2019.
 //  Copyright © 2019 Starknet Technologies®. All rights reserved.
-//
+
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,17 +14,52 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         // Override point for customization after application launch.
+        let configuration = ParseClientConfiguration {
+            
+            
+            
+        }
+        
+        Parse.initialize(with: configuration)
+        saveInstallationObject()
+        
         return true
     }
+    
+    func saveInstallationObject(){
+        if let installation = PFInstallation.current(){
+            installation.saveInBackground {
+                (success: Bool, error: Error?) in
+                if (success) {
+                    print("You have successfully connected your app to Back4App!")
+                } else {
+                    if let myError = error{
+                        print(myError.localizedDescription)
+                    }else{
+                        print("Uknown error")
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
@@ -40,7 +74,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
 
 }
 
