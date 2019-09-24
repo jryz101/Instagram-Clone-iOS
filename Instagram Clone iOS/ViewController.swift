f45d69d9f96a45d6dedd054e1f75ce9286b1ec9c
JerryTan <jryz101@gmail.com>
2019-09-24 19:33:07 +0800

    Initial Commit

---
+++ Instagram Clone iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram Clone iOS
+//
+//  Created by Jerry Tan on 24/09/2019.
+//  Copyright © 2019 Starknet Technologies®. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
