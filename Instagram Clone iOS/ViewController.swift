ed1445b59892df51a44b6ace95ca09fd75094bdc
JerryTan <jryz101@gmail.com>
2019-09-25 06:42:57 +0800

    1.7. Import 'Parse' framework in ViewController. 1.8. Create 'displayAlert' function. 1.9. Compose sign up methods in 'signupOrLogin' function block.

--- Instagram Clone iOS/ViewController.swift
+++ Instagram Clone iOS/ViewController.swift
@@ -5,11 +5,30 @@
 
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
     
     ////check sign up mode is active variable
     var signupModeisActive = true
+    
+    
+    
+    
+    //MARK: - display alert function
+    func displayAlert(title: String, message: String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+            
+            self.dismiss(animated: true, completion: nil)
+            
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+
+    }
     
     
     
@@ -24,8 +43,50 @@ class ViewController: UIViewController {
     //MARK: - sign up or login action and outlet
     @IBOutlet weak var signupOrLoginOutlet: UIButton!
     @IBAction func signupOrLogin(_ sender: Any) {
+        
+        ////methods for checking error input and display alert message
+        if email.text == "" || password.text == "" {
+            
+            displayAlert(title: "Error In Form", message: "Please enter an email and pasword")
+            
+            
+        } else {
+            
+            ////methods for signing up user with email and password
+            if (signupModeisActive) {
+                
+                let user = PFUser()
+                
+                user.username = email.text
+                user.password = password.text
+                user.email = email.text
+                
+                ////Signs up the user *asynchronously*
+                user.signUpInBackground { (success, error) in
+                    
+                    if let error = error {
+                        
+                        self.displayAlert(title:"Could Not Sign Up", message: error.localizedDescription)
+                        
+                    } else {
+                        
+                        print("Signing Up")
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
     
     }
+    
+    
+    
+    
+    
+    
     
     
     //MARK: - switch to login action and outlet
