1bc76e4e730b0e8e8fb21f5cfc55f1f97636cfea
JerryTan <jryz101@gmail.com>
2019-10-07 00:20:43 +0800

    Working on PostViewController: 6.4. Import ‘Parse’ framework in ‘PostViewController’. 6.5. Create new Parse PFObject class "Post" in post image action. 6.6. Declare sender post message 'comment’ and userid ‘objectId' in post image action. 6.7. Create imageData and imageFile data and add imageFile to post in post image action. 6.8. Declare save post in background completion block in post image action. 6.9. Reset comment and image to post to default value and declare display alert message for image successfully posted and could not be posted. 7.0. Declare spinner activity indicator after 'imageData' section and the beginning of the post.saveInBackground completion block.

--- Instagram Clone iOS/PostViewController.swift
+++ Instagram Clone iOS/PostViewController.swift
@@ -5,6 +5,7 @@
 
 
 import UIKit
+import Parse
 
 class PostViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
@@ -48,11 +49,79 @@ class PostViewController: UIViewController, UINavigationControllerDelegate, UIIm
     
     
     
+    //MARK: - display alert function
+    func displayAlert(title: String, message: String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+            
+            self.dismiss(animated: true, completion: nil)
+            
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
     
     
     
     //MARK: - post image action
     @IBAction func postImage(_ sender: Any) {
+        
+        if let image = imageToPost.image {
+        
+        ////new PFObject class "Post"
+        let post = PFObject(className: "Post")
+        
+        ////sender userid and message which is comment and objectId
+        post["message"] = comment.text
+        post["userid"] = PFUser.current()?.objectId
+            
+            ////image data | returns the data for the specified image in PNG format
+            if let imageData = image.pngData() {
+                
+                ////spinner activity indicator
+                let activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+                activityIndicator.center = self.view.center
+                activityIndicator.hidesWhenStopped = true
+                activityIndicator.style = UIActivityIndicatorView.Style.gray
+                view.addSubview(activityIndicator)
+                activityIndicator.startAnimating()
+                UIApplication.shared.beginIgnoringInteractionEvents()
+                
+                ////image file | `PFFileObject` representes a file of binary data stored on the Parse servers
+                let imageFile = PFFileObject(name: "image.png", data: imageData)
+                
+                ////add image file to post
+                post["imageFile"] = imageFile
+                
+                ////save post in background
+                post.saveInBackground { (success, error) in
+                    
+                    ////Stops the animation of the progress indicator
+                    activityIndicator.stopAnimating()
+                    UIApplication.shared.endIgnoringInteractionEvents()
+                    
+                    if success {
+                        
+                        //// image posted successfully message
+                        self.displayAlert(title: "Image Posted", message: "Your image has been posted successfully")
+                        
+                        ////reset comment and image to post to default
+                        self.comment.text = ""
+                        self.imageToPost.image = nil
+                        
+                    } else {
+                        
+                        //// image could not be posted message
+                        self.displayAlert(title: "Image could not be posted", message: "Please try again")
+                    }
+                }
+            }
+        }
     }
     
     
