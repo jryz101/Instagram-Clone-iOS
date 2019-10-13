3005cb69e7264c54607ee1cf43e3903240fae708
JerryTan <jryz101@gmail.com>
2019-10-14 02:33:34 +0800

    Working on FeedTableViewController: 7.9. Import Parse framework on FeedTableViewController and create a query which returns objects of this type in view did load section. 8.0. Add a constraint to the query that requires a particular key's ‘object’ (username) to be not equal to the provided ‘object’ (current username). 8.1. Declare findObjectsInBackground to finds ‘ objects' (username in Parse Server) *asynchronously* and calls the given block with the results. 8.2. Declare if we can let ‘users’ equals to object, and then, create a for in loop method to loop through ‘object' in ‘users’. 8.3. Casting PFObject to PFUser, and Update users dictionary and store username with objectId. 8.4. Create getFollowerUserQuery for searching all the user that our current user follows. 8.5. Declare getFollowerUserQuery to get the current user Follower objectId. 8.6. Declare query.findObjectsInBackground completion block and finds objects *asynchronously* and calls the given block with the results. 8.7. Declare the 'PFQuery’ class to defines a query that is used to query for `PFObject`s. 8.8. Add a 'whereKey' constraint to the query that requires a particular key’s ‘userid' object to be equal to the provided object. 8.9. Declare query.findObjectsInBackground completion block to append comments, username, and imageFiles data to Parse server data table which is ‘message’, ‘userid’, and ‘imageFile’. 9.0. Edit numberOfRowsInSection to return ‘comments.count’ and edit cell.comment.text equals to comments[indexPath.row], and cell.userInfo.text equals to usernames[indexPath.row]. 9.1. Finally, write a 'getDataInBackground' methods to get the image data. Asynchronously* gets the data from cache if available or fetches its contents from the network.

--- Instagram Clone iOS/FeedTableViewController.swift
+++ Instagram Clone iOS/FeedTableViewController.swift
@@ -5,14 +5,92 @@
 
 
 import UIKit
+import Parse
 
 class FeedTableViewController: UITableViewController {
+    
+    //MARK: - variable section
+    var users = [String: String]()
+    var comments = [String]()
+    var usernames = [String]()
+    var imageFiles = [PFFileObject]()
+    
+    
+    
+    
     
     
     //MARK: - view did load section
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        ////create a query which returns objects of this type
+        let query = PFUser.query()
+        
+        ////add a constraint to the query that requires a particular key's object to be not equal to the provided object
+        query?.whereKey("username", notEqualTo: PFUser.current()?.username as Any)
+        
+        ////finds objects *asynchronously* and calls the given block with the results
+        query?.findObjectsInBackground(block: { (objects, error) in
+            
+            if let users = objects {
+                
+                for object in users {
+                    
+                    ////casting PFObject to PFUser
+                    if let user = object as? PFUser {
+                        
+                        ////update user array and store username with objectId on user dictionary
+                        self.users[user.objectId!] = user.username!
+                        
+                    }
+                    
+                }
+                
+            }
+            ////getFollowerUserQuery for searching all the user that our current user follows
+            let getFollowerUserQuery = PFQuery(className: "FollowingRelation")
+            
+            ////get the current user Follower objectId
+            getFollowerUserQuery.whereKey("Follower", equalTo: PFUser.current()?.objectId as Any)
+            
+            ///finds objects *asynchronously* and calls the given block with the results.
+            getFollowerUserQuery.findObjectsInBackground(block: { (objects, error) in
+                
+                if let followers = objects {
+                    
+                    for follower in followers {
+                        
+                        if let followedUser = follower["Following"] {
+                            
+                            ////the 'PFQuery' class defines a query that is used to query for `PFObject`s.
+                            let query = PFQuery(className: "Post")
+                            
+                            ////add a constraint to the query that requires a particular key's object to be equal to the provided object.
+                            query.whereKey("userid", equalTo: followedUser)
+                            
+                            query.findObjectsInBackground(block: { (objects, error) in
+                                
+                                if let posts = objects {
+                                    
+                                    for post in posts {
+                                    
+                                        ////append comments, usernames, and imagefile values to Parse server data table
+                                        self.comments.append(post["message"] as! String)
+                                        self.usernames.append(self.users[post["userid"] as! String]!)
+                                        self.imageFiles.append(post["imageFile"] as! PFFileObject)
+                                        
+                                        
+                                        self.tableView.reloadData()
+                                        
+                                    }
+                                }
+                            })
+                        }
+                    }
+                }
+            })
+        })
     }
     
     
@@ -30,7 +108,7 @@ class FeedTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         // #warning Incomplete implementation, return the number of rows
-        return 4
+        return comments.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -41,9 +119,22 @@ class FeedTableViewController: UITableViewController {
         self.tableView.rowHeight = 300
         
         // Configure the cell...
-        cell.postedImage.image = UIImage(named: "Demo Image.png")
-        cell.comment.text = "Comment"
-        cell.userInfo.text = "Username"
+        ////asynchronously* gets the data from cache if available or fetches its contents from the network.
+        imageFiles[indexPath.row].getDataInBackground { (data, error) in
+            
+            if let imageData = data {
+                
+                if let imageToDisplay = UIImage(data: imageData) {
+                    
+                    cell.postedImage.image = imageToDisplay
+                    
+                }
+            }
+        }
+        
+        cell.comment.text = comments[indexPath.row]
+        
+        cell.userInfo.text = usernames[indexPath.row]
 
         return cell
     }
